1289ba08305c9dc6d9020ee767636fe3acf131d2
Daniel J <danielimmanj@gmail.com>
2024-09-21 13:51:31 +0530

    added logs

--- order-service/src/main/java/com/ecom/order/service/OrderServiceKafkaListener.java
+++ order-service/src/main/java/com/ecom/order/service/OrderServiceKafkaListener.java
@@ -10,6 +10,7 @@ public class OrderServiceKafkaListener {
 
     @KafkaListener(topics = "user-registered", groupId = "order-service-group", containerFactory = "listenerContainerFactory")
     public void handleUserRegisteredEvent(UserRegisteredEvent event) {
+        log.info("Received User Registered Event for user: {}", event);
         log.info("Received User Registered Event for user ID: {}", event.getUserId());
         log.info("User snapshot saved in Order Service database for user ID: {}", event.getEmail());
     }
