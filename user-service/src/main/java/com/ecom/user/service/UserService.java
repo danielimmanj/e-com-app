aaaf19572b0eebc424025d86cbd5c74d7ce2a018
Daniel J <danielimmanj@gmail.com>
2024-09-21 19:29:07 +0530

    circuit breaker added for user service

--- user-service/src/main/java/com/ecom/user/service/UserService.java
+++ user-service/src/main/java/com/ecom/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.ecom.user.config.kafka.KafkaProducer;
 import com.ecom.user.dto.EventDto;
 import com.ecom.user.model.User;
 import com.ecom.user.repository.UserRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,7 +28,15 @@ public class UserService {
         return savedUser;
     }
 
+    @CircuitBreaker(name = "userService", fallbackMethod = "fallbackGetUserByMail")
     public User getUserByMail(String mail) {
-        return userRepository.findByEmail(mail).orElseThrow();
+        return userRepository.findByEmail(mail)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+
+    // Fallback method for circuit breaker
+    public User fallbackGetUserByMail(String mail, Throwable throwable) {
+        System.out.println("Fallback triggered due to: " + throwable.getMessage());
+        return new User(); // Return a default user or handle error accordingly
     }
 }
