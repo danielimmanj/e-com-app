7e3d44a79795a2fdc7c6566dee0e2d1e037f366d
DanielDorustree <daniel@dorustree.in>
2024-10-02 14:31:10 +0530

    dynamic image with unsplash

--- shopfront-ui/src/app/app.config.ts
+++ shopfront-ui/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    provideStore(), // Include your store provider if using NgRx
+  ],
 };
