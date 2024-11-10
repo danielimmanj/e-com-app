71b1419899a335c45a3ea35a606ee34ca5955844
Daniel <danielimmanj@gmail.com>
2024-11-10 15:51:19 +0530

    wish list icon added

--- shopfront-ui/src/app/components/product/product.component.ts
+++ shopfront-ui/src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../model/product.model';
 import { CommonModule } from '@angular/common';
+import { WishlistService } from '../../services/wishlist/wishlist.service';
 
 @Component({
   selector: 'app-product',
@@ -9,6 +10,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   @Input() product!: Product;
+  isInWishlist = false;
+  constructor(private wishlistService: WishlistService) {}
+
+  ngOnInit(): void {
+    this.isInWishlist = this.wishlistService.isInWishlist(this.product);
+  }
+
+  toggleWishlist(): void {
+    if (this.isInWishlist) {
+      this.wishlistService.removeFromWishlist(this.product);
+    } else {
+      this.wishlistService.addToWishlist(this.product);
+    }
+    this.isInWishlist = !this.isInWishlist;
+  }
 }
