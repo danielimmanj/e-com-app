f5d270b3184a587202e31b5fd211de6737224493
Daniel <danielimmanj@gmail.com>
2025-05-24 20:05:07 +0530

    env files renamed

--- shopfront-ui/src/app/services/product/product.service.ts
+++ shopfront-ui/src/app/services/product/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../../model/product.model';
-import { environment } from '../../../environments/environments';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
